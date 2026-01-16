59d72fad68ff1b41632073f10bc25a88644abece
Ternegroup <huanternehall@gmail.com>
2026-01-16 13:05:03 +0100

    Create Tsign.py

---
+++ Tsign.py
@@ -0,0 +1,12 @@
+[out:json][timeout:240];
+// fetch area “göteborg” to search in
+{{geocodeArea:göteborg}}->.searchArea;
+// schools in area
+nwr(area.searchArea)["amenity"="school"]->.schools;
+// Find traffic signs with Swedish codes and inscriptions referencing time or "Skola"
+(
+  node(area.searchArea)["traffic_sign"~"SE:C31|SE:A13"];     // speed limit or children warning
+)->.signs;
+ 
+// print results
+out geom;
